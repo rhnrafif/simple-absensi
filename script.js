dc4456e2e36e627836ec54103c826ce2db2f2538
rhnrafif <raihanudinrafif@gmail.com>
2022-09-02 22:49:58 +0700

    1 file modified

--- script.js
+++ script.js
@@ -1,63 +1,66 @@
 //menangkap isi dari element dari form
-let absensi_form = document.getElementById("absensi_form");
-let root = document.getElementById("root");
-
+let absensi_form = document.getElementById('absensi_form');
+let root = document.getElementById('root');
 
 //membuat array untuk menampung data absensi
 let absensi_data = [];
 
 // tambahkan event listener submit ke element absensi_form
-absensi_form.addEventListener("submit", (event)=>{
- 
-    //mencegah form dari reload page
-    event.preventDefault();
+absensi_form.addEventListener('submit', (event) => {
+  //mencegah form dari reload page
+  event.preventDefault();
 
-    let fullname = event.target.fullname.value;
+  let fullname = event.target.fullname.value;
 
-    //mini validasi
-    if( fullname == ""){
-        alert('Silahkan masukan nama bang...');
-        return
-    }
+  //mini validasi
+  if (fullname == '') {
+    alert('Silahkan masukan nama bang...');
+    return;
+  }
 
-    //push data ke dalam array absensi_data
-    absensi_data.push({
-        nama_lengkap : fullname, //mengambil value dari input
-        waktu : new Date().toLocaleTimeString(), //function javascript untuk waktu
-        tanggal : new Date().toLocaleDateString() //function javascript untuk tanggal
-    });
+  //push data ke dalam array absensi_data
+  absensi_data.push({
+    nama_lengkap: fullname, //mengambil value dari input
+    waktu: new Date().toLocaleTimeString(), //function javascript untuk waktu
+    tanggal: new Date().toLocaleDateString(), //function javascript untuk tanggal
+  });
 
-    //reset input field
-    event.target.fullname.value = ""; //mengakses input menggunakan event
+  //reset input field
+  event.target.fullname.value = ''; //mengakses input menggunakan event
 
-    //panngil function render HTML
-    renderToHtml();
+  //panngil function render HTML
+  renderToHtml();
 });
 
 //render semua data ke dalam tampilan HTML
-function renderToHtml(){
-    //reset element div root
-    root.innerHTML = "";
+function renderToHtml() {
+  //reset element div root
+  root.innerHTML = '';
 
-    //mapping array to html element
-    absensi_data.forEach((e, i)=>{
-        root.innerHTML += `
+  //mapping array to html element
+  absensi_data.forEach((e, i) => {
+    root.innerHTML += `
         <div class="card" draggable="true" ondragend="handleDelete(${i})" >
         <span>${i + 1} . &nbsp; ${e.nama_lengkap}</span>  
         <span> ${e.waktu} ${e.tanggal}</span>
         </div>
-        `
-        // //i+1 adalah untuk mencegah agar index tidak ditampilkan dari 0
-    });
+        `;
+    // //i+1 adalah untuk mencegah agar index tidak ditampilkan dari 0
+  });
 }
 
-
 //delete function
-function handleDelete(i){
+function handleDelete(i) {
+  //alert untuk confirmasi delete
+  let confirmDelete = confirm('yakin delete?');
 
-    //delete 1 data dari array
-    absensi_data.splice(i, 1);
+  if (!confirmDelete) {
+    return;
+  }
 
-    //render kembali element nya
-    renderToHtml();
+  //delete 1 data dari array
+  absensi_data.splice(i, 1);
+
+  //render kembali element nya
+  renderToHtml();
 }
